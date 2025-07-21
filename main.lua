d53983f902ec06fa618182bef7b0aeacce69b123
realjay2 <ajtheprince2020@gmail.com>
2025-07-21 01:57:38 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
 if game.PlaceId == 8204899140 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c07b4a952d231796c162e4f594776472.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realjay2/ZenHub/refs/heads/main/FF2.lua"))()
